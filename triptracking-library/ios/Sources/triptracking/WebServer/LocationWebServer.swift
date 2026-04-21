dd738bbe75fe82aa8090e851cb9d172aedd380d3
Hieu Nguyen <hieu.nguyen@sw.innova.com>
2026-04-21 13:07:57 +0700

    Update webview other port

--- triptracking-library/ios/Sources/triptracking/WebServer/LocationWebServer.swift
+++ triptracking-library/ios/Sources/triptracking/WebServer/LocationWebServer.swift
@@ -12,7 +12,7 @@ import Network
 public class LocationWebServer {
     
     private var listener: NWListener?
-    private var port: UInt16 = 8080
+    private var port: UInt16 = 8081
     private let fallbackPorts: [UInt16] = [8080, 8081, 8082, 8083]
     private var connections: [NWConnection] = []
     
